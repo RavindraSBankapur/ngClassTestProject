bcd94fc50282d35e6f24ed64fef349647afb8718
RavindraSBankapur <rsbravindra@gmail.com>
2021-02-03 10:37:20 +0530

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { products } from "../products";
 @Component({
-  selector: 'app-product-details',
-  templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"]
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private route: ActivatedRoute) {}
+  product;
   ngOnInit() {
-  }
+    // const routeParams = this.route.snapshot.paramMap;
+    // const productIdFromRoute = Number(routeParams.get("productId"));
 
+    // this.product = products.find(product => product.id === productIdFromRoute);
+    this.route.paramMap.subscribe(
+      params => {
+        this.product = products[+params.get("productId")];
+      },
+      err => {
+        console.log("Some error");
+      }
+    );
+  }
 }
