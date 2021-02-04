21480f5c575c8dfebc96912ef22bb1c79ff93afb
RavindraSBankapur <rsbravindra@gmail.com>
2021-02-04 12:06:51 +0530

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -14,6 +14,7 @@
 
 import { Component, OnInit } from "@angular/core";
 import { CartService } from "../cart.service";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-cart",
@@ -23,7 +24,14 @@ import { CartService } from "../cart.service";
 export class CartComponent implements OnInit {
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private router: Router) {}
 
   ngOnInit() {}
+
+  // this is to demonstrate router by angular Router
+  routeTo404() {
+    this.router.navigateByUrl("**");
+  }
+  routeTo500() {}
+  routeToGeneric() {}
 }
